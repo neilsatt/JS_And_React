6751a13d8914248e063e688d29ac6c62adadea08
neilsatt <neilsatt@gmail.com>
2017-10-01 19:36:18 -0600

    add component section

---
+++ React/Basics/ThinkingComponents/scoreboard/app.jsx
@@ -0,0 +1,43 @@
+function Application() {
+  return (
+    <div className="scoreboard">
+         <div className="header">
+            <h1>Scoreboard</h1>
+          </div> 
+          
+          <div className="players">
+            <div className="player">
+               <div className="player-name">  
+                 Jim Bob
+                </div>
+                <div className="player-score">
+                   <div className="counter">
+                    <button className="counter-action decrement"> - </button>
+                       <div className="counter-score"> 31 </div>
+                    <button className="counter-action increment"> + </button>
+                   </div> 
+                
+                </div>
+            </div>
+          </div>
+          
+        <div className="players">
+            <div className="player">
+               <div className="player-name">  
+                 Terry Standord
+                </div>
+                <div className="player-score">
+                   <div className="counter">
+                    <button className="counter-action decrement"> - </button>
+                       <div className="counter-score"> 55 </div>
+                    <button className="counter-action increment"> + </button>
+                   </div> 
+                
+                </div>
+            </div>
+          </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
