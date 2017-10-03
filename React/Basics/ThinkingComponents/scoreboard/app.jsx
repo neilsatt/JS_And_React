cf6db01bd9bbb210eb0fe023c277ae2922a0929e
neilsatt <neilsatt@gmail.com>
2017-10-02 18:54:14 -0600

    components update

--- React/Basics/ThinkingComponents/scoreboard/app.jsx
+++ React/Basics/ThinkingComponents/scoreboard/app.jsx
@@ -1,8 +1,8 @@
-function Application() {
+function Application(props) {
   return (
     <div className="scoreboard">
          <div className="header">
-            <h1>Scoreboard</h1>
+            <h1>{props.title}</h1>
           </div> 
           
           <div className="players">
@@ -38,6 +38,16 @@ function Application() {
           </div>
     </div>
   );
+}
+
+
+
+Application.propTypes = {
+    title: React.PropTypes.string,
+};
+
+Application.defaultProps = {
+    title: "Scoreboard",
 }
 
 ReactDOM.render(<Application />, document.getElementById('container'));
